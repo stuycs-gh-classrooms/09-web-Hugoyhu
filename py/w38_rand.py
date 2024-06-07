387894b85fb342b73834a0806d16e3dc0d8c10c8
Hugo Hu <hhu60@stuy.edu>
2024-06-07 14:23:34 -0400

    add gitignore

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -4,3 +4,24 @@ print('Content-type: text/html\n')
 from random import random
 
 print('hello, ', random())
+
+from datetime import date, datetime
+
+print (
+f'''
+
+<head><title>random w38</title></head>
+<body style="background-color:pink;">
+    <p>
+        Hello! This is a random number: {random()}
+        <br>
+        Today, it is {datetime.now().strftime("%A, %B %d. It is %I:%M %p.")}
+    </p>
+</body>
+
+
+
+
+
+'''
+)
